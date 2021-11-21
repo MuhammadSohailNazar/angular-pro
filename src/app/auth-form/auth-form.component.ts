4221593c9a5cfcbd3df1af08d525d7c42d2ae40a
Muhammad Sohail Nazar <sohail@tcig.co>
2021-11-21 15:56:09 +0300

    Creating Dynamic component. 1. setup entry point 2. inject componentfactoryresolver 3. create an instance of that component 4. because use of read viewcontainerref, that allow as to creat component.

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,19 +1,10 @@
 import {
-  AfterContentInit,
-  AfterViewInit,
   ChangeDetectorRef,
   Component,
-  ContentChildren,
-  ElementRef,
   EventEmitter,
   Output,
-  QueryList,
   Renderer2,
-  ViewChild,
-  ViewChildren,
 } from '@angular/core';
-import { AuthMessageComponent } from '../auth-message/auth-message.component';
-import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 import { IUser } from './User';
 
 @Component({
@@ -21,37 +12,11 @@ import { IUser } from './User';
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss'],
 })
-export class AuthFormComponent implements AfterContentInit, AfterViewInit {
-  @ContentChildren(AuthRememberComponent)
-  remembers: QueryList<AuthRememberComponent>;
-  @ViewChildren(AuthMessageComponent) messages: QueryList<AuthMessageComponent>;
-  @ViewChild('email') email: ElementRef;
+export class AuthFormComponent {
+  title: string = 'Login';
   @Output('submitted') submitted = new EventEmitter<IUser>();
 
-  showMessage: boolean = false;
-  constructor(private cd: ChangeDetectorRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit() {
-    this.messages.forEach((message) => {
-      message.days = 30;
-    });
-    this.cd.detectChanges();
-    this.renderer.setAttribute(this.email.nativeElement,"placeholder","Enter your email address");
-    this.renderer.addClass(this.email.nativeElement,"email");
-    this.renderer.selectRootElement(this.email.nativeElement).focus();
-    //this.email.nativeElement.setAttribute('placeholder', 'Enter your email');
-    // this.email.nativeElement.classList.add('email');
-    // this.email.nativeElement.focus();
-
-  }
-
-  ngAfterContentInit() {
-    this.remembers.forEach((item) => {
-      item.checked.subscribe(
-        (checked: boolean) => (this.showMessage = checked)
-      );
-    });
-  }
+  constructor() {}
 
   onSubmit(form: any): void {
     this.submitted.emit(form);
