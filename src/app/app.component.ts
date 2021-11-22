ac3afbaeba3202c79ae40428ff1b5b553a8c2514
Muhammad Sohail Nazar <sohail@tcig.co>
2021-11-23 00:03:05 +0300

    inject ng template using ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,8 @@
-import {
-  AfterViewInit,
-  Component,
-  TemplateRef,
-  ViewChild,
-  ViewContainerRef,
-} from '@angular/core';
-import { IUser } from './auth-form/User';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild("tmpl") tmpl: TemplateRef<any>;
-  constructor(
-  ) {}
-
-
-  ngAfterViewInit() {
-    this.entry.createEmbeddedView(this.tmpl,{
-      $implicit: "sohail nazar",
-      location: "Karachi"
-    });
-  }
-
-  
-
-  login(user: IUser) {
-    console.log('login', user);
-  }
-}
+export class AppComponent {}
