fffa2787822e5cf25317ad1faeb385ebb3aa8049
Muhammad Sohail Nazar <sohail@tcig.co>
2021-11-23 13:36:11 +0300

    Hostlistener implementation using directive.

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[credit-card]'
 })
 export class CreditCardDirective {
+  @HostListener('input', ['$event'])
+   onKeyDown(event : KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    let trimmed = input.value.replace(/\s+/g, ''); 
+    if (trimmed.length > 16) {
+      trimmed = trimmed.substr(0, 16);
+    }
+    let numbers = [];
+    for (let i = 0; i < trimmed.length; i += 4) {
+      numbers.push(trimmed.substr(i, 4));
+    }
 
-  constructor(private element: ElementRef) {
-    console.log(this.element);
-   }
-
+    input.value = numbers.join(' ');
+  }
 }
