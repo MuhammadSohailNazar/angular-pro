4db8e3a3901f5c54caa6251646f44c11ad300f28
Muhammad Sohail Nazar <sohail@tcig.co>
2021-11-24 01:05:35 +0300

    how to create custom directive and use export as attribute.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreditCardDirective } from './credit-card/credit-card.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
-  declarations: [AppComponent, CreditCardDirective],
+  declarations: [AppComponent, CreditCardDirective, TooltipDirective],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
