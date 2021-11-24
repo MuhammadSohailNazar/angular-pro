83051f8dc12394b68cae5561e42c448c6992714a
Muhammad Sohail Nazar <sohail@tcig.co>
2021-11-24 13:29:44 +0300

    Reactive form basic setup.

--- src/app/stock-inventory/container/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/container/stock-inventory/stock-inventory.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-stock-inventory',
   templateUrl: './stock-inventory.component.html',
-  styleUrls: ['./stock-inventory.component.scss']
+  styleUrls: ['./stock-inventory.component.scss'],
 })
 export class StockInventoryComponent implements OnInit {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('B182'),
+      code: new FormControl('1223'),
+    }),
+  });
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit() {
+    console.log(this.form.value);
   }
-
 }
